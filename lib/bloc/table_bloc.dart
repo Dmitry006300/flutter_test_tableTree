5f5f1ccf9d471aa0e639758a2d8df6ae0756afa8
home <dolphin-d@ya.ru>
2025-02-20 09:31:41 +0400

    bloc concurrency

--- lib/bloc/table_bloc.dart
+++ lib/bloc/table_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test_table/bloc/table_event.dart';
 import 'package:flutter_test_table/bloc/table_state.dart';
@@ -5,59 +6,54 @@ import 'package:flutter_test_table/models/table_cell.dart';
 
 class TableBloc extends Bloc<TableEvent, TableState> {
   TableBloc() : super(TableState(rootElements: [])) {
-    on<AddRootElement>((event, emit) {
-      final newRootElements = List<TableElement>.from(state.rootElements)
-        ..add(TableElement(text: "Новый корневой элемент"));
-      emit(state.copyWith(rootElements: newRootElements));
-    });
-
-    on<UpdateElement>((event, emit) {
-      event.node.textEditingController.text = event.node.text;
-      emit(state.copyWith(rootElements: List.from(state.rootElements)));
-    });
-
-    on<ElementExpansion>((event, emit) {
-      event.node.isExpanded = !event.node.isExpanded;
-      emit(state.copyWith());
-    });
-
-    on<DeleteElement>((event, emit) {
-      void deleteNode(List<TableElement> nodes, TableElement target) {
-        nodes.removeWhere((node) => node == target);
-        for (var node in nodes) {
-          deleteNode(node.children, target);
-        }
-      }
-
-      final updatedRootElements = List<TableElement>.from(state.rootElements);
-      deleteNode(updatedRootElements, event.node);
-      emit(state.copyWith(rootElements: updatedRootElements));
-    });
-
-    on<AddChildElement>((event, emit) {
-      event.parentNode.children
-          .add(TableElement(text: "Новый вложенный элемент"));
-      emit(state.copyWith());
-    });
-
-    void updateParentCheckboxes(TableElement currentNode) {
-      void updateParent(TableElement node, List<TableElement> elements) {
-        for (TableElement parent in elements) {
-          if (parent.children.contains(node)) {
-            parent.isChecked = parent.children.any((child) => child.isChecked);
-            updateParent(parent, state.rootElements);
-            break;
+    on<TableEvent>((event, emit) async {
+      if (event is AddRootElement) {
+        on<AddRootElement>((event, emit) {
+          final newRootElements = List<TableElement>.from(state.rootElements)
+            ..add(TableElement(text: "Новый корневой элемент"));
+          emit(state.copyWith(rootElements: newRootElements));
+        });
+      } else if (event is UpdateElement) {
+        event.node.textEditingController.text = event.node.text;
+        emit(state.copyWith());
+      } else if (event is ElementExpansion) {
+        event.node.isExpanded = !event.node.isExpanded;
+        emit(state.copyWith());
+      } else if (event is DeleteElement) {
+        void deleteNode(List<TableElement> nodes, TableElement target) {
+          nodes.removeWhere((node) => node == target);
+          for (var node in nodes) {
+            deleteNode(node.children, target);
           }
-          updateParent(node, parent.children);
         }
+
+        final updatedRootElements = List<TableElement>.from(state.rootElements);
+        deleteNode(updatedRootElements, event.node);
+        emit(state.copyWith(rootElements: updatedRootElements));
+      } else if (event is AddChildElement) {
+        event.parentNode.children
+            .add(TableElement(text: "Новый вложенный элемент"));
+        emit(state.copyWith());
+      } else if (event is UpdateParentCheckboxes) {
+        void updateParentCheckboxes(TableElement currentNode) {
+          void updateParent(TableElement node, List<TableElement> elements) {
+            for (TableElement parent in elements) {
+              if (parent.children.contains(node)) {
+                parent.isChecked =
+                    parent.children.any((child) => child.isChecked);
+                updateParent(parent, state.rootElements);
+                break;
+              }
+              updateParent(node, parent.children);
+            }
+          }
+
+          updateParent(currentNode, state.rootElements);
+        }
+
+        updateParentCheckboxes(event.node);
+        emit(state.copyWith(rootElements: List.from(state.rootElements)));
       }
-
-      updateParent(currentNode, state.rootElements);
-    }
-
-    on<UpdateParentCheckboxes>((event, emit) {
-      updateParentCheckboxes(event.node);
-      emit(state.copyWith(rootElements: List.from(state.rootElements)));
-    });
+    }, transformer: sequential());
   }
 }
